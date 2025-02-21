ad1578f3b87e4b644ccc06becd0b5457822c4745
DUSHIME1212 <mdonavan33@gmail.com>
2025-02-21 15:24:46 +0200

    update product image URLs in Shop model

--- lib/models/Shop.dart
+++ lib/models/Shop.dart
@@ -9,28 +9,28 @@ class Shop extends ChangeNotifier {
       description: 'A red shirt - it is pretty red!',
       price: 29.99,
       imagePath:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://i.pinimg.com/736x/38/24/b2/3824b281c386fa011e59f5a3dade0943.jpg',
     ),
     Product(
       name: 'Blue Shirt',
       description: 'A blue shirt - it is pretty blue!',
       price: 29.99,
       imagePath:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://i.pinimg.com/736x/c7/01/d1/c701d15fff6dd7fa23f2c2d284d099bd.jpg',
     ),
     Product(
       name: 'Yellow Shirt',
       description: 'A yellow shirt - it is pretty yellow!',
       price: 29.99,
       imagePath:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://i.pinimg.com/736x/75/b5/1e/75b51e7de0a8f744ce5bf34e89769b7f.jpg',
     ),
     Product(
       name: 'Green Shirt',
       description: 'A green shirt - it is pretty green!',
       price: 29.99,
       imagePath:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://i.pinimg.com/736x/68/49/5c/68495c85832f767a72a3cf31108cea80.jpg',
     ),
   ];
 
